2409a90903c4d104bd3307a5b5e80026bb5076ea
asyncKate <zdeb.katarzyna@gmail.com>
2026-01-07 14:17:15 +0100

    #cleaning

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,6 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import "./styles/utilities.scss";
 import "./styles/globals.css";
-import "./i18n";
+import "./i18n.ts";
 
 createRoot(document.getElementById("root")!).render(<App />);  
